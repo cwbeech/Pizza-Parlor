01de2fa678bbfd5ead14733c1fe46c17b6f5870d
cwbeech <cwbeech@k-state.edu>
2023-11-10 16:01:11 -0600

    Finished Milestone 9

---
+++ Data/Menu.cs
@@ -0,0 +1,200 @@
+﻿/* Menu.cs
+ * Author: Calvin Beechner
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaParlor.Data
+{
+	/// <summary>
+	/// Menu class.
+	/// </summary>
+	public static class Menu
+	{
+		/// <summary>
+		/// Private list made for simplifying later code.
+		/// </summary>
+		private static Size[] _sizes = { Size.Small, Size.Medium, Size.Large };
+
+		/// <summary>
+		/// Private list made for simplifying later code.
+		/// </summary>
+		private static Crust[] _crusts = { Crust.Thin, Crust.Original, Crust.DeepDish };
+
+		/// <summary>
+		/// Private list made for simplifying later code.
+		/// </summary>
+		private static SodaFlavor[] _sodaFlavors = { SodaFlavor.Coke, SodaFlavor.Sprite, SodaFlavor.DrPepper, SodaFlavor.RootBeer, SodaFlavor.DietCoke };
+
+		/// <summary>
+		/// Private list made for simplifying later code.
+		/// </summary>
+		private static WingSauce[] _sauces = { WingSauce.Mild, WingSauce.Medium, WingSauce.Hot, WingSauce.HoneyBBQ };
+
+		/// <summary>
+		/// List of all possible pizzas.
+		/// </summary>
+		public static IEnumerable<IMenuItem> Pizzas
+		{
+			get
+			{
+				List<IMenuItem> result = new List<IMenuItem>();
+				//Pizza
+				for (int s = 0; s < 3; s++)
+				{
+					for (int c = 0; c < 3; c++)
+					{
+						Pizza p = new Pizza();
+						p.PizzaSize = _sizes[s];
+						p.PizzaCrust = _crusts[c];
+						result.Add(p);
+					}
+				}
+				//HawaiianPizza
+				for (int s = 0; s < 3; s++)
+				{
+					for (int c = 0; c < 3; c++)
+					{
+						HawaiianPizza p = new HawaiianPizza();
+						p.PizzaSize = _sizes[s];
+						p.PizzaCrust = _crusts[c];
+						result.Add(p);
+					}
+				}
+				//MeatsPizza
+				for (int s = 0; s < 3; s++)
+				{
+					for (int c = 0; c < 3; c++)
+					{
+						MeatsPizza p = new MeatsPizza();
+						p.PizzaSize = _sizes[s];
+						p.PizzaCrust = _crusts[c];
+						result.Add(p);
+					}
+				}
+				//SupremePizza
+				for (int s = 0; s < 3; s++)
+				{
+					for (int c = 0; c < 3; c++)
+					{
+						SupremePizza p = new SupremePizza();
+						p.PizzaSize = _sizes[s];
+						p.PizzaCrust = _crusts[c];
+						result.Add(p);
+					}
+				}
+				//VeggiePizza
+				for (int s = 0; s < 3; s++)
+				{
+					for (int c = 0; c < 3; c++)
+					{
+						VeggiePizza p = new VeggiePizza();
+						p.PizzaSize = _sizes[s];
+						p.PizzaCrust = _crusts[c];
+						result.Add(p);
+					}
+				}
+				return result;
+			}
+		}
+
+		/// <summary>
+		/// List of all possible sides.
+		/// </summary>
+		public static IEnumerable<IMenuItem> Sides
+		{
+			get
+			{
+				List<IMenuItem> result = new List<IMenuItem>();
+
+				//Breadsticks
+				for (int c = 0; c < 2; c++)
+				{
+					Breadsticks b = new Breadsticks();
+					b.Cheese = (c == 1);
+					result.Add(b);
+				}
+				//CinnamonSticks
+				for (int f = 0; f < 2; f++)
+				{
+					CinnamonSticks c = new CinnamonSticks();
+					c.Frosting = (f == 1);
+					result.Add(c);
+				}
+				//GarlicKnots
+				result.Add(new GarlicKnots());
+				//Wings
+				for (int b = 0; b < 2; b++)
+				{
+					for (int s = 0; s < 4; s++)
+					{
+						Wings w = new Wings();
+						w.BoneIn = (b == 1);
+						w.Sauce = _sauces[s];
+						result.Add(w);
+					}
+				}
+
+				return result;
+			}
+		}
+
+		/// <summary>
+		/// List of all possible drinks.
+		/// </summary>
+		public static IEnumerable<IMenuItem> Drinks
+		{
+			get
+			{
+				List<IMenuItem> result = new List<IMenuItem>();
+				//IcedTea
+				for (int s = 0; s < 3; s++)
+				{
+					IcedTea i = new IcedTea();
+					i.DrinkSize = _sizes[s];
+					result.Add(i);
+				}
+				//Soda
+				for (int s = 0; s < 3; s++)
+				{
+					for (int f = 0; f < 5; f++)
+					{
+						Soda d = new Soda();
+						d.DrinkSize = _sizes[s];
+						d.DrinkType = _sodaFlavors[f];
+						result.Add(d);
+					}
+				}
+
+				return result;
+			}
+		}
+
+		/// <summary>
+		/// List of all items on the menu.
+		/// </summary>
+		public static IEnumerable<IMenuItem> FullMenu
+		{
+			get
+			{
+				List<IMenuItem> result = new List<IMenuItem>();
+				foreach (IMenuItem p in Pizzas)
+				{
+					result.Add(p);
+				}
+				foreach (IMenuItem s in Sides)
+				{
+					result.Add(s);
+				}
+				foreach (IMenuItem d in Drinks)
+				{
+					result.Add(d);
+				}
+				return result;
+			}
+		}
+	}
+}
---
+++ DataTests/MenuUnitTests.cs
@@ -0,0 +1,250 @@
+﻿/* MenuUnitTests.cs
+ * Author: Calvin Beechner
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaParlor.DataTests
+{
+    /// <summary>
+    /// Menu Unit Tests.
+    /// </summary>
+    public class MenuUnitTests
+    {
+        /// <summary>
+        /// Tests that the number of pizzas equals 45;
+        /// </summary>
+        [Fact]
+        public void CountPizzasEquals45()
+        {
+            Assert.Equal(45, Menu.Pizzas.Count());
+        }
+
+        /// <summary>
+        /// Tests that the number of sides equals 13;
+        /// </summary>
+        [Fact]
+        public void CountSidesEquals13()
+        {
+            Assert.Equal(13, Menu.Sides.Count());
+        }
+        /// <summary>
+        /// Tests that the number of drinks equals 18;
+        /// </summary>
+        [Fact]
+        public void CountDrinksEquals18()
+        {
+            Assert.Equal(18, Menu.Drinks.Count());
+        }
+        /// <summary>
+        /// Tests that the number of menu items equals 76;
+        /// </summary>
+        [Fact]
+        public void CountFullMenuEquals76()
+        {
+            Assert.Equal(76, Menu.FullMenu.Count());
+        }
+
+        /// <summary>
+        /// Tests that the pizzas list has each unique combination.
+        /// </summary>
+        [Fact]
+        public void PizzasHasUniqueCombinations()
+        {
+            //Pizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is Pizza && (item as Pizza).PizzaSize == _sizes[s] && (item as Pizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //HawaiianPizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is HawaiianPizza && (item as HawaiianPizza).PizzaSize == _sizes[s] && (item as HawaiianPizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //MeatsPizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is MeatsPizza && (item as MeatsPizza).PizzaSize == _sizes[s] && (item as MeatsPizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //SupremePizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is SupremePizza && (item as SupremePizza).PizzaSize == _sizes[s] && (item as SupremePizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //VeggiePizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is VeggiePizza && (item as VeggiePizza).PizzaSize == _sizes[s] && (item as VeggiePizza).PizzaCrust == _crusts[c]);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Tests that the sides list has each unique combination.
+        /// </summary>
+        [Fact]
+        public void SidesHasUniqueCombinations()
+        {
+            //Breadsticks
+            Assert.Contains(Menu.Sides, item => item is Breadsticks && (item as Breadsticks).Cheese == false);
+            Assert.Contains(Menu.Sides, item => item is Breadsticks && (item as Breadsticks).Cheese == false);
+            //CinnamonSticks
+            Assert.Contains(Menu.Sides, item => item is CinnamonSticks && (item as CinnamonSticks).Frosting == false);
+            Assert.Contains(Menu.Sides, item => item is CinnamonSticks && (item as CinnamonSticks).Frosting == false);
+            //GarlicKnots
+            Assert.Contains(Menu.Sides, item => item is GarlicKnots);
+            //Wings
+            for (int s = 0; s < 4; s++)
+            {
+                for (int bonein = 0; bonein < 2; bonein++)
+                {
+                    Assert.Contains(Menu.Sides, item => item is Wings && (item as Wings).Sauce == _sauces[s] && (item as Wings).BoneIn == (1 == bonein));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests that the drinks list has each unique combination.
+        /// </summary>
+        [Fact]
+        public void DrinksHasUniqueCombinations()
+        {
+            //Soda
+            for (int s = 0; s < 3; s++)
+            {
+                for (int d = 0; d < 5; d++)
+                {
+                    Assert.Contains(Menu.Drinks, item => item is Soda && (item as Soda).DrinkSize == _sizes[s] && (item as Soda).DrinkType == _sodaFlavors[d]);
+                }
+            }
+            //IcedTea
+            for (int s = 0; s < 3; s++)
+            {
+                Assert.Contains(Menu.Drinks, item => item is IcedTea && (item as IcedTea).DrinkSize == _sizes[s]);
+            }
+        }
+
+        /// <summary>
+        /// Tests that the full menu list has each unique combination.
+        /// </summary>
+        [Fact]
+        public void FullMenuHasUniqueCombinations()
+        {
+            //Pizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is Pizza && (item as Pizza).PizzaSize == _sizes[s] && (item as Pizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //HawaiianPizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is HawaiianPizza && (item as HawaiianPizza).PizzaSize == _sizes[s] && (item as HawaiianPizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //MeatsPizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is MeatsPizza && (item as MeatsPizza).PizzaSize == _sizes[s] && (item as MeatsPizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //SupremePizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is SupremePizza && (item as SupremePizza).PizzaSize == _sizes[s] && (item as SupremePizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //VeggiePizza
+            for (int s = 0; s < 3; s++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Assert.Contains(Menu.Pizzas, item => item is VeggiePizza && (item as VeggiePizza).PizzaSize == _sizes[s] && (item as VeggiePizza).PizzaCrust == _crusts[c]);
+                }
+            }
+            //Breadsticks
+            Assert.Contains(Menu.Sides, item => item is Breadsticks && (item as Breadsticks).Cheese == false);
+            Assert.Contains(Menu.Sides, item => item is Breadsticks && (item as Breadsticks).Cheese == false);
+            //CinnamonSticks
+            Assert.Contains(Menu.Sides, item => item is CinnamonSticks && (item as CinnamonSticks).Frosting == false);
+            Assert.Contains(Menu.Sides, item => item is CinnamonSticks && (item as CinnamonSticks).Frosting == false);
+            //GarlicKnots
+            Assert.Contains(Menu.Sides, item => item is GarlicKnots);
+            //Wings
+            for (int s = 0; s < 4; s++)
+            {
+                for (int bonein = 0; bonein < 2; bonein++)
+                {
+                    Assert.Contains(Menu.Sides, item => item is Wings && (item as Wings).Sauce == _sauces[s] && (item as Wings).BoneIn == (1 == bonein));
+                }
+            }
+            //Soda
+            for (int s = 0; s < 3; s++)
+            {
+                for (int d = 0; d < 5; d++)
+                {
+                    Assert.Contains(Menu.Drinks, item => item is Soda && (item as Soda).DrinkSize == _sizes[s] && (item as Soda).DrinkType == _sodaFlavors[d]);
+                }
+            }
+            //IcedTea
+            for (int s = 0; s < 3; s++)
+            {
+                Assert.Contains(Menu.Drinks, item => item is IcedTea && (item as IcedTea).DrinkSize == _sizes[s]);
+            }
+        }
+
+        #region Private arrays.
+        /// <summary>
+		/// Private list made for simplifying later code.
+		/// </summary>
+		private Size[] _sizes = { Size.Small, Size.Medium, Size.Large };
+
+        /// <summary>
+        /// Private list made for simplifying later code.
+        /// </summary>
+        private Crust[] _crusts = { Crust.Thin, Crust.Original, Crust.DeepDish };
+
+        /// <summary>
+        /// Private list made for simplifying later code.
+        /// </summary>
+        private SodaFlavor[] _sodaFlavors = { SodaFlavor.Coke, SodaFlavor.Sprite, SodaFlavor.DrPepper, SodaFlavor.RootBeer, SodaFlavor.DietCoke };
+
+        /// <summary>
+        /// Private list made for simplifying later code.
+        /// </summary>
+        private WingSauce[] _sauces = { WingSauce.Mild, WingSauce.Medium, WingSauce.Hot, WingSauce.HoneyBBQ };
+
+        /// <summary>
+        /// Private array made for simlifying later code.
+        /// </summary>
+        private Pizza[] _pizzas = { new Pizza(), new HawaiianPizza(), new MeatsPizza(), new SupremePizza(), new VeggiePizza() };
+
+        #endregion
+    }
+}
